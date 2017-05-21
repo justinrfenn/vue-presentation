7348780cf130a90c79bfa83eaf71da73904848be
Justin-Fenn <jrfenn1824@gmail.com>
2017-05-21 18:33:05 -0400

    2 - Wire up client with socket to server

--- Notes.md
+++ Notes.md
@@ -2,4 +2,16 @@
   * `npm install -g vue-cli`
   * `vue init <template-name> <project-name>`
 
-Credit to Eric for javascript course and chat app homework. Get permission to display name first
+* Let's make a simple message stream that we can post a message to. This will require wiring up socket.io. Let's do that first.
+  * As a note, I will be using port 8081 for the client application and 8082 for the server.
+  * The focus of this is not the server, but let me briefly explain what it does.
+  * `npm install fetch --save`
+
+* Let's set a data property to be a list for the messages
+* let's create a computed property to get the list of messages
+  * include start up wiring and show pitfall 
+  * move on to beforeCreate to wire up socket.io room
+
+What's the difference between computed and method? 
+* Computed properties are cache based and will only reevaluate when some it's dependencies change
+* methods will always run
--- chat-app-with-tutorial/src/App.vue
+++ chat-app-with-tutorial/src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <ul>
+      <li v-for="message in messages">
+         {{ message.message }}
+      </li>
+    </ul>
+    <input type="text" @keyup.enter="sendMessage">
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import io from 'socket.io-client'
 
 export default {
   name: 'app',
-  components: {
-    Hello
-  }
+  data: function() {
+    return {
+      messages: [],
+      socket: {}
+    }
+  },
+  beforeCreate() {
+    let room = "sample";
+    let baseUrl = "http://localhost:8082";
+    let url = baseUrl + "/api/rooms/" + room;
+
+    fetch(url, {method: 'put'})
+    .then(() => {
+      this.socket = io.connect(baseUrl).io.socket('/rooms/' + room);
+      fetch(url)
+      .then(res => res.json())
+      .then(room => {
+        room.messages.forEach( msg => this.messages.push(msg));
+        // subscribe to new messages
+        this.socket.on("new", msg => this.messages.push(msg));
+      })
+    })
+  },
+  methods: {
+    sendMessage: function(event) {
+      let messageText = event.target.value;
+      this.socket.emit("send", messageText);
+      event.target.value = "";
+    }
+  } 
+
 }
 </script>
 
